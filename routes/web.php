18d69ffc9deda239ac05f2b5c84528018757b3d9
Elian-0P <naufal17112004@gmail.com>
2025-06-01 01:04:04 +0700

    crud done?

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InternshipApplicationController;
+use App\Http\Controllers\Api\InternshipApplicationApiController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('internships', InternshipApplicationController::class);
+});
+
+Route::get('/api/internships', [InternshipApplicationApiController::class, 'index']);
+Route::get('/api/internships/{id}', [InternshipApplicationApiController::class, 'show']);
+
+require __DIR__.'/auth.php';
