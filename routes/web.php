52df22a70dbd07811b2a3de8cdc97a4fbd718d6b
Elian-0P <naufal17112004@gmail.com>
2025-06-02 23:31:56 +0700

    removing breeze to prevent conflict

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InternshipApplicationController;
 use App\Http\Controllers\Api\InternshipApplicationApiController;
@@ -9,21 +8,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::resource('internships', InternshipApplicationController::class)->middleware('auth');
 
-Route::middleware(['auth'])->group(function () {
-    Route::resource('internships', InternshipApplicationController::class);
-});
 
 Route::get('/api/internships', [InternshipApplicationApiController::class, 'index']);
 Route::get('/api/internships/{id}', [InternshipApplicationApiController::class, 'show']);
 
-require __DIR__.'/auth.php';
